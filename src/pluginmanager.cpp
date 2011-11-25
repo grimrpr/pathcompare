e7f67e2a0ce3e1ee354449cfe1088995ea92d0f6
Benjamin Aschenbrenner <benjamin.aschenbrenner@gmail.com>
2011-11-25 09:44:29 +0100

    adding plugin loader functionality

--- src/pluginmanager.cpp
+++ src/pluginmanager.cpp
@@ -1,11 +1,67 @@
 #include "pluginmanager.h"
 
-PluginManager::PluginManager(QObject *parent) :
-    QObject(parent)
+PluginManager::PluginManager(const QDir &searchdir, QTabWidget *tw, QObject *parent) :
+    QObject(parent),
+        plugin_dir(searchdir),
+        tab_widget(tw)
 {
+        //only look at lib files
+        QStringList strl;
+        strl << "*.so" << "*.dll" << "*.sl";
+        plugin_dir.setNameFilters(strl);
+
 }
 
 void PluginManager::addPlugin(const ComperatorPluginPtr &comparator)
 {
         plugins << comparator;
+}
+
+void PluginManager::updatePlugins()
+{
+
+        QStringList new_plugin_list = plugin_dir.entryList();
+
+        QStringList::ConstIterator it;
+        for(it = new_plugin_list.constBegin(); it < new_plugin_list.constEnd(); ++it)
+        {
+                //plugin has not been loaded yet
+                if(!plugin_list.contains(*it))
+                {
+                        //try to load plugin
+                    //load all plugins found in plugin_dir and add them a tab in tw
+                    QPluginLoader plugin_loader_main(*it);
+
+                    if(plugin_loader_main.load())
+                    {
+                            QObject * object = plugin_loader_main.instance();
+                            ComperatorPluginFactoryInterface *cpfi = qobject_cast<ComperatorPluginFactoryInterface *>(object);
+                            if(cpfi){
+                                    //add new tab
+                                    int tab_index = tab_widget->addTab();
+
+
+                                    plugin_names_list << cpfi->getPluginName();
+
+                                    //set tab text to plugin name
+                                    tab_widget->setTabText(tab_index, cpfi->getPluginName());
+
+                                    std::cout << "plugin: "
+                                              << cpfi->getPluginName().toLocal8Bit().constData()
+                                              << "has been loaded "
+                                              << std::endl;
+
+                                    ComperatorPluginPtr comp_plugin = cpfi->createComperatorPlugin(&ros_mngr,
+                                                                                                   tab_widget->widget(tab_index));
+                                    plugin_list << comp_plugin;
+
+                            }
+                            else
+                                    qDebug() << "cast to ComperatorPluginFactoryInterface was not possible";
+                    }
+                    else
+                            qDebug() << plugin_loader_main.errorString();
+
+                }
+        }
 }
--- src/pluginmanager.h
+++ src/pluginmanager.h
@@ -4,6 +4,9 @@
 //Qt includes
 #include <QObject>
 #include <QList>
+#include <QDir>
+#include <QTabWidget>
+#include <QPluginLoader>
 
 //project includes
 #include "interfaces/comperatorplugin.h"
@@ -12,15 +15,19 @@
 class PluginManager : public QObject
 {
         Q_OBJECT
-        QList<ComperatorPluginPtr> plugins;
+        QList<ComperatorPluginPtr> plugin_list;
+        QList<QString> plugin_names_list;
+        QDir plugin_dir;
+        QTabWidget *tab_widget;
 
 public:
-        explicit PluginManager(QObject *parent = 0);
+        explicit PluginManager(const QDir &searchdir, QTabWidget * tw, QObject *parent = 0);
         void addPlugin(const ComperatorPluginPtr &comparator);
 
 Q_SIGNALS:
 
 public Q_SLOTS:
+        void updatePlugins();
 
 };
 
